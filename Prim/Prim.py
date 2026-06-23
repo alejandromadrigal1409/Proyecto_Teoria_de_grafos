3403e1db0b8b166cdf349142b31053752a9a9160
Ale <alejandromadrigal1409@gmail.com>
2026-06-23 13:08:27 -0600

    mejora en prim

--- Prim/Prim.py
+++ Prim/Prim.py
@@ -1,35 +1,37 @@
 import networkx as nx
 
 def primAlgorithm(G):
-  T = nx.Graph()
-  visitedComponent = set()
 
-  # crear lista de adyacencias del grafo
-  adjList = {node: {} for node in G.nodes}
-  for edge in G.edges:
-    adjList[edge[0]][edge[1]] = G[edge[0]][edge[1]]["weight"]
-    adjList[edge[1]][edge[0]] = G[edge[1]][edge[0]]["weight"]
+    if len(G.nodes) == 0:
+        return nx.Graph()
 
-  # seleccionar un nodo
-  u = list(G.nodes)[0]
-  visitedComponent.add(u)
+    T = nx.Graph()
 
-  while len(visitedComponent) < len(G.nodes):
-    aux = {}
-    for u in visitedComponent:
-      aux[u] = adjList[u]
+    start = list(G.nodes)[0]
 
-    minWeight = float('inf')
-    minOut = None
-    edgeStart = None
-    for i in aux:
-      for node, weight in aux[i].items():
-        if weight < minWeight and node not in visitedComponent:
-          minWeight = weight
-          minOut = node
-          edgeStart = i
-    T.add_edge(edgeStart, minOut, weight=minWeight)
-  
-    visitedComponent.add(minOut)
-    
-  return T
+    visited = {start}
+    T.add_node(start)
+
+    while len(visited) < len(G.nodes):
+
+        minWeight = float("inf")
+        edgeStart = None
+        edgeEnd = None
+
+        for u in visited:
+            for v, attrs in G[u].items():
+
+                weight = attrs["weight"]
+
+                if v not in visited and weight < minWeight:
+                    minWeight = weight
+                    edgeStart = u
+                    edgeEnd = v
+
+        if edgeEnd is None:
+            raise ValueError("El grafo no es conexo")
+
+        T.add_edge(edgeStart, edgeEnd, weight=minWeight)
+        visited.add(edgeEnd)
+
+    return T
